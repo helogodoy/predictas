32533dae2e878b8800139263969345093ea0ccf1
Heloisa Godoy <godoymariaheloisa@gmail.com>
2025-10-27 11:09:01 -0300

    conexão do bd mysql (criação de pastas)

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// conexão MySQL
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+// testa conexão
+db.connect((err) => {
+  if (err) {
+    console.error("❌ Erro ao conectar no MySQL:", err);
+  } else {
+    console.log("✅ Conectado ao banco MySQL com sucesso!");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Servidor Node conectado ao MySQL 🚀");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
