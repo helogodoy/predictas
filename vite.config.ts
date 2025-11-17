17817b4faf9845f83938e2f14715d9557399a34d
Heloisa Godoy <godoymariaheloisa@gmail.com>
2025-11-17 10:19:13 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/predictas/", 
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // backend local
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
